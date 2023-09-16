1d9f3aca779fc0bf88940059189c961106ed5f0f
Nabeeb Yusuf <101312514+nabeeb-yusuf@users.noreply.github.com>
2023-09-16 18:41:54 +1000

    added unit tests for `WorkersManager`

--- Leaf/database-tests/AdminsManager.test.ts
+++ Leaf/database-tests/AdminsManager.test.ts
@@ -1,18 +1,32 @@
+/**
+ * @jest-environment node
+ */
+import { DatabaseCollection } from "../src/database/DatabaseCollection";
+import DatabaseSession from "../src/database/DatabaseSession";
 import Admin from "../src/model/employee/Admin";
 import AdminsManager from "../src/model/session/AdminsManager";
+import NewEmployeeManager from "../src/model/session/NewEmployeeManager";
+
+const admin = Admin.new("John", "Doe"); // Create an admin instance
 
 describe("Admin Manager", () => {
-    it("should add an Admin to the database", () => {
-        const admin = Admin.new("John", "Doe"); // Create an admin instance
+    it("should add an Admin to the database", async () => {
+        const testPromise = await NewEmployeeManager.inst.newAdminCreated(admin); // Insert admin to database
 
-        const testPromise = AdminsManager.inst.updateAdmin(admin); // Insert admin to database
+        expect(testPromise).toBe(true); // Check if successfully inserted admin to Database
+    });
 
-        expect(testPromise).resolves.toBe(true); // Check if successfully inserted admin to Database
+    it("should retrieve an Admin from the database", async () => {
+        const retrievedAdmin = await AdminsManager.inst.getAdmin(admin.id); // retrieve the admin from the database
 
-        const retrievedAdmin = AdminsManager.inst.getAdmin(admin.id); // retrieve the admin from the database
-
-        expect(retrievedAdmin).resolves.toBeInstanceOf(Admin); // Check if retrieved object is an instance of Admin
-
-
-    })
-});
+        if (retrievedAdmin != null) {
+            expect(retrievedAdmin.id).toEqual(admin.id); // check if id matches
+            expect(retrievedAdmin.fullName).toEqual(admin.fullName); // check if name matches
+        }
+    });
+        
+    it("should delete an Admin from the database", async () => {
+        const deletePromise = await DatabaseSession.inst.delete(DatabaseCollection.Admins, admin.id.toString()); // Delete entry from database
+        expect(deletePromise).toBe(true);
+    });
+})
---
+++ Leaf/database-tests/WorkersManager.test.ts
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment node
+ */
+
+import { DatabaseCollection } from "../src/database/DatabaseCollection";
+import DatabaseSession from "../src/database/DatabaseSession";
+import Worker from "../src/model/employee/Worker"
+import NewEmployeeManager from "../src/model/session/NewEmployeeManager";
+import WorkersManager from "../src/model/session/WorkersManager";
+
+const worker = Worker.new("John", "Doe", null);
+
+describe("Worker Manager", () => {
+    it("should add a Worker to the database", async () => {
+        const testPromise = await NewEmployeeManager.inst.newWorkerCreated(worker); // Insert Worker to the database
+
+        expect(testPromise).toBe(true); // Check if insert successful
+    })
+
+    it("should retrieve a Worker from the database", async () => {
+        const retrievedWorker = await WorkersManager.inst.getWorker(worker.id); // Retrieve worker from the database
+
+        if (retrievedWorker != null) {
+            expect(retrievedWorker.id).toEqual(worker.id); // Check if retrieved worker id matches expected worker id
+            expect(retrievedWorker.fullName).toEqual(worker.fullName); // Check if retrieved worker name matches expected worker name
+        }
+    })
+
+    it("should delete a Worker from the database", async () => {
+        const deletePromise = await DatabaseSession.inst.delete(DatabaseCollection.Workers, worker.id.toString()); // Delete entry from database
+        expect(deletePromise).toBe(true); // Check if successfully deleted
+    })
+})
